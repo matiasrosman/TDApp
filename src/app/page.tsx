42ca263ab5caa84bbe3d82bcaebe4855e2b85aea
Lautarosz <110540088+Lautarosz@users.noreply.github.com>
2024-06-25 12:03:37 -0300

    adelantos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@
 
 import * as React from "react"
 import { Calendar } from "./_components/Calendar"
+import "../styles/globals.css";
 
 export default function CalendarDemo() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
-    
+    <html lang="en" >
+       
+     
+       <body> 
+        <header >
+       <title> Calendario </title>
+       <h1 className="titulo"> Calendario </h1>
+       </header>
         <div className="Calendario">
           <Calendar
       mode="single"
@@ -16,5 +24,9 @@ export default function CalendarDemo() {
       />
         </div>
       
+    </body>   
+    </html>
+   
+  
   )
 }
