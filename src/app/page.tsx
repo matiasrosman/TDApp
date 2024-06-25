05555a445726435ffe5f8918b30eca66f73bed2c
Lautarosz <110540088+Lautarosz@users.noreply.github.com>
2024-06-25 11:15:59 -0300

    div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,15 @@ import { Calendar } from "./_components/Calendar"
 export default function CalendarDemo() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
-    <Calendar
+    
+        <div className="Calendario">
+          <Calendar
       mode="single"
       selected={date}
       onSelect={setDate}
       className="rounded-md border"
+      />
+        </div>
       
-    />
   )
 }
